d29937586dd434fbc84781fabc4d11a1c639e07b
Federico Miras <miras.federico@gmail.com>
2018-03-15 04:18:01 -0300

    Add 1 more test case of req.url change

--- test/index.js
+++ test/index.js
@@ -4,17 +4,28 @@ const { sleep } = require('sleep')
 
 const cache = require('../lib')
 
-test('cache(1000, <Function>) execute the handler twice', async t => {
+test('cache(...args) execute the handler twice with 3 requests', async t => {
   const handler = sinon.stub()
   handler.onCall(0).returns(1)
   handler.onCall(1).returns(2)
 
   const microFn = cache(1000, handler)
-  const req = { url: 'https://github.com' }
+  const req = { url: 'https://someapi.com' }
 
   t.is(1, microFn(req))
   t.is(1, microFn(req))
   await sleep(1)
   t.is(2, microFn(req))
   t.is(2, handler.callCount)
+})
+
+test('cache(...args) execute the handler if different url', t => {
+  const handler = sinon.stub()
+  handler.onCall(0).returns(1)
+  handler.onCall(1).returns(2)
+
+  const microFn = cache(1000, handler)
+
+  t.is(1, microFn({ url: 'https://someapi.com/resource/1' }))
+  t.is(2, microFn({ url: 'https://someapi.com/resource/2' }))
 })
