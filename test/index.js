deae8a9aa7b6c2050392722f5381b3a11bd1723b
Federico Miras <miras.federico@gmail.com>
2018-03-15 18:17:53 -0300

    Support Mongo usage to horizontal scaling

--- test/index.js
+++ test/index.js
@@ -9,23 +9,23 @@ test('cache(...args) execute the handler twice with 3 requests', async t => {
   handler.onCall(0).returns(1)
   handler.onCall(1).returns(2)
 
-  const microFn = cache(1000, handler)
+  const microFn = await cache(1000, handler)
   const req = { url: 'https://someapi.com' }
 
-  t.is(1, microFn(req))
-  t.is(1, microFn(req))
+  t.is(1, await microFn(req))
+  t.is(1, await microFn(req))
   await sleep(1)
-  t.is(2, microFn(req))
+  t.is(2, await microFn(req))
   t.is(2, handler.callCount)
 })
 
-test('cache(...args) execute the handler if different url', t => {
+test('cache(...args) execute the handler if different url', async t => {
   const handler = sinon.stub()
   handler.onCall(0).returns(1)
   handler.onCall(1).returns(2)
 
   const microFn = cache(1000, handler)
 
-  t.is(1, microFn({ url: 'https://someapi.com/resource/1' }))
-  t.is(2, microFn({ url: 'https://someapi.com/resource/2' }))
+  t.is(1, await microFn({ url: 'https://someapi.com/resource/1' }))
+  t.is(2, await microFn({ url: 'https://someapi.com/resource/2' }))
 })
